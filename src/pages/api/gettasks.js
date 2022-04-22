d1c876dba12de6a5d63f86a65018fec6c20c8140
Hari kumar <hari70319@gmail.com>
2022-04-22 18:49:32 +0530

    Removing files

--- src/pages/api/gettasks.js
+++
@@ -1,9 +0,0 @@
-import db from "../../utils/db";
-import Task from "../../models/Task";
-
-export default async function handler(req, res) {
-  await db.connect();
-  const tasks = await Task.find({});
-  await db.disconnect();
-  res.status(200).json({ success: true, tasks });
-}
